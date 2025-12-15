d75a41c65dc7da46f57d923ae580924d39fdd186
Vdtry-06 <tunhoipro0306@gmail.com>
2025-12-15 20:00:57 +0700

    update order

--- services/order/src/main/java/com/vdtry06/ecommerce/OrderApplication.java
+++ services/order/src/main/java/com/vdtry06/ecommerce/OrderApplication.java
@@ -2,12 +2,16 @@ package com.vdtry06.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "com.vdtry06.ecommerce")
+@EnableDiscoveryClient
 public class OrderApplication {
 
 	public static void main(String[] args) {
--- services/order/src/main/java/com/vdtry06/ecommerce/order/OrderRepository.java
+++ services/order/src/main/java/com/vdtry06/ecommerce/order/OrderRepository.java
@@ -1,7 +1,9 @@
 package com.vdtry06.ecommerce.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 
 }
