31871e74875c1ff89b54cf72677011e5d133e1f3
Vdtry-06 <tunhoipro0306@gmail.com>
2025-12-15 20:00:27 +0700

    update product

--- services/product/src/main/java/com/vdtry06/ecommerce/ProductApplication.java
+++ services/product/src/main/java/com/vdtry06/ecommerce/ProductApplication.java
@@ -2,8 +2,12 @@ package com.vdtry06.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.vdtry06.ecommerce")
+@EnableDiscoveryClient
 public class ProductApplication {
 
 	public static void main(String[] args) {
